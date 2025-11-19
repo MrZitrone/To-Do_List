9428dfe6e5095723b066faeb3a693517c9408a45
MrZitrone <antoslav2004@gmail.com>
2025-11-19 01:51:47 +0100

    Update

--- To-Do List/obj/Debug/net8.0/To-Do List.AssemblyInfo.cs
+++ To-Do List/obj/Debug/net8.0/To-Do List.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("To-Do List")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+67d08528a52aa0783a1f94cf4c179f7b7ad19700")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6b5b1f85245a0568973facda5b94251ab015c223")]
 [assembly: System.Reflection.AssemblyProductAttribute("To-Do List")]
 [assembly: System.Reflection.AssemblyTitleAttribute("To-Do List")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
